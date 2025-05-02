44cfdfb1414525d651270d118c1d4e7ab837f390
Valentin Leistner <git@valentinleistner.com>
2025-05-02 10:49:02 +0200

    fix axum route capture syntax

--- src/endpoints/mod.rs
+++ src/endpoints/mod.rs
@@ -22,30 +22,33 @@ pub fn entity_routes<E: Entity<S>, S: ContextTrait>() -> Router<S> {
             &format!("/api/v1/{name_pl}"),
             get(api::get_entities::<E, S>),
         )
-        .route(&format!("/api/v1/{name}/:id"), get(api::get_entity::<E, S>))
+        .route(
+            &format!("/api/v1/{name}/{{id}}"),
+            get(api::get_entity::<E, S>),
+        )
         .route(
             &format!("/api/v1/{name_pl}"),
             post(api::post_entities::<E, S>),
         )
         .route(
-            &format!("/api/v1/{name}/:id"),
+            &format!("/api/v1/{name}/{{id}}"),
             post(api::post_entity::<E, S>),
         )
         .route(
-            &format!("/api/v1/{name}/:id"),
+            &format!("/api/v1/{name}/{{id}}"),
             delete(api::delete_entity::<E, S>),
         )
         // UI
         .route(&format!("/{name_pl}"), get(ui::get_entities::<E, S>))
-        .route(&format!("/{name}/:id"), get(ui::get_entity::<E, S>))
-        .route(&format!("/{name}/:id"), post(ui::post_entity::<E, S>))
+        .route(&format!("/{name}/{{id}}"), get(ui::get_entity::<E, S>))
+        .route(&format!("/{name}/{{id}}"), post(ui::post_entity::<E, S>))
         .route(&format!("/{name_pl}/add"), get(ui::get_add_entity::<E, S>))
         .route(
             &format!("/{name_pl}/add"),
             post(ui::post_add_entity::<E, S>),
         )
         .route(
-            &format!("/{name}/:id/delete"),
+            &format!("/{name}/{{id}}/delete"),
             post(ui::delete_entity::<E, S>),
         )
 }
