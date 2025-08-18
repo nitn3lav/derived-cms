bacbba63929912c7d20a8ca7b3533df437766171
Valentin Leistner <git@valentinleistner.com>
2025-08-18 17:42:00 +0200

    add feature vendored-js-lib that skips using npm during build (enabled by default)

--- build.rs
+++ build.rs
@@ -1,19 +1,8 @@
-use std::{fs, path::Path, process::Command};
-
 use walkdir::WalkDir;
 
 fn main() {
-    Command::new("npm")
-        .arg("install")
-        .status()
-        .expect("Failed to run `npm install`")
-        .success()
-        .then_some(())
-        .unwrap();
-
-    include_file("node_modules/easymde/dist/easymde.min.js");
-    include_file("node_modules/easymde/dist/easymde.min.css");
-    include_file("node_modules/sortablejs/Sortable.min.js");
+    #[cfg(not(feature = "vendored-js-lib"))]
+    npm::fetch_npm();
 
     for e in WalkDir::new("i18n") {
         println!(
@@ -23,11 +12,30 @@ fn main() {
     }
 }
 
-fn include_file(path: impl AsRef<Path>) {
-    let dest = Path::new("static").join(&path);
-    if let Some(dir) = dest.parent() {
-        fs::create_dir_all(dir).expect("Failed to create dir");
+#[cfg(not(feature = "vendored-js-lib"))]
+mod npm {
+    use std::{fs, path::Path, process::Command};
+
+    pub fn fetch_npm() {
+        Command::new("npm")
+            .arg("install")
+            .status()
+            .expect("Failed to run `npm install`")
+            .success()
+            .then_some(())
+            .unwrap();
+
+        include_file("node_modules/easymde/dist/easymde.min.js");
+        include_file("node_modules/easymde/dist/easymde.min.css");
+        include_file("node_modules/sortablejs/Sortable.min.js");
     }
-    fs::copy(path.as_ref(), dest).expect("Failed to copy file");
-    println!("cargo:rerun-if-changed={}", path.as_ref().to_str().unwrap());
+
+    fn include_file(path: impl AsRef<Path>) {
+        let dest = Path::new("static").join(&path);
+        if let Some(dir) = dest.parent() {
+            fs::create_dir_all(dir).expect("Failed to create dir");
+        }
+        fs::copy(path.as_ref(), dest).expect("Failed to copy file");
+        println!("cargo:rerun-if-changed={}", path.as_ref().to_str().unwrap());
+    }
 }
